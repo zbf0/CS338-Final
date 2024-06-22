1b9fd36d8904124107d72090d43c3c639697f1a4
zbf0 <129675911+zbf0@users.noreply.github.com>
2024-06-21 20:00:39 -0400

    Update read-sample.sql (Pre-create tables for our database)

--- DatabaseSetup/read-sample.sql
+++ DatabaseSetup/read-sample.sql
@@ -31,3 +31,54 @@ CREATE TABLE users (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+-- create table for viewingHistory
+
+CREATE TABLE viewingHistory (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId INT NOT NULL,
+    titleId varchar(256),
+    watched_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- create table for contactForm
+
+CREATE TABLE contactForm (
+    cid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    date INT NOT NULL,
+    isComplete INT NOT NULL,
+    userId INT,
+    SId INT,
+    content VARCHAR(1000)
+);
+
+-- create table for userAuthentication
+
+CREATE TABLE userAuthentication (
+    userid INT NOT NULL UNIQUE
+    username VARCHAR(50)
+    email VARCHAR(50)
+);
+
+-- create table for staff
+
+CREATE TABLE staff (
+    SId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    SFname VARCHAR(50),
+    SLname VARCHAR(50)
+);
+
+-- create table for User comment/rating
+
+CREATE TABLE comment (
+    commentId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    rating INT,
+    comment VARCHAR(1000),
+    commentAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    userid INT NOT NULL,
+    tconst VARCHAR(256)
+);
+
+-- add more later
