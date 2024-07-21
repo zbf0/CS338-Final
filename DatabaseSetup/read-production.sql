6bfe4ce312ea654a0293f26922b93dd9051a3568
Owen <129675911+zbf0@users.noreply.github.com>
2024-07-21 02:24:57 -0400

    Add files via upload

--- DatabaseSetup/read-production.sql
+++ DatabaseSetup/read-production.sql
@@ -12,16 +12,39 @@ CREATE TABLE titleAkas (
     types varchar(256),
     attributes varchar(256),
     isOriginalTitle int
+    
 );
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/title.akas.csv'
 INTO TABLE titleakas
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 CREATE INDEX idx_titleId ON titleAkas (titleId);
 CREATE INDEX idx_title ON titleAkas (title);
+
+-- read from title.basics.csv
+
+CREATE TABLE titleBasics (
+    titleId varchar(256),
+    titleType varchar(256),
+    primaryTitle varchar(560),
+    originalTitle varchar(560),
+    isAdult INT,
+    startYear varchar(4),
+    endYear varchar(4),
+    runtimeMinutes varchar(16),
+    genres TEXT
+);
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/title.basics.csv'
+INTO TABLE titleBasics
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+ESCAPED BY '\"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(titleId, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres, @dummy, @dummy, @dummy, @dummy, @dummy);
 
 -- read from title.ratings.csv
 CREATE TABLE titleRatings (
@@ -33,10 +56,73 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/title.ratings.cs
 INTO TABLE titleRatings
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 CREATE INDEX idx_titleId ON titleRatings (titleId);
+
+-- read from title.episode.csv
+CREATE TABLE titleEpisode (
+    titleId varchar(256),
+    parentId varchar(256),
+    seasonNum INT,
+    episodeNum INT
+);
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/title.episode.csv'
+INTO TABLE titleEpisode
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+-- read from title.crew.csv
+CREATE TABLE titleCrew (
+    titleId varchar(256),
+    directorId TEXT,
+    writerId LONGTEXT
+);
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/title.crew.csv'
+INTO TABLE titleCrew
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+-- read from title.principals.csv
+CREATE TABLE titlePrincipals (
+    titleId varchar(256),
+    ordering INT,
+    personId varchar(256),
+    category varchar(256),
+    job varchar(256),
+    characters TEXT
+);
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/title.principals.csv'
+INTO TABLE titlePrincipals
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(titleId, ordering, personId, category, job, characters, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy);
+
+-- read from name.basics.csv
+CREATE TABLE nameBasics (
+    personId varchar(256),
+    primaryName varchar(256),
+    birthYear varchar(4),
+    deathYear varchar(4),
+    primaryProfession varchar(256),
+    knownForTitles varchar(256)
+);
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/name.basics.csv'
+INTO TABLE nameBasics
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(personId, primaryName, birthYear, deathYear, primaryProfession, knownForTitles, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy);
+
+
 
 -- create table for users
 
@@ -59,7 +145,6 @@ CREATE TABLE viewingHistory (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     userId INT NOT NULL,
     titleId varchar(256) NOT NULL,
-    ordering INT NOT NULL,
     date VARCHAR(21) NOT NULL
 );
 
@@ -85,8 +170,33 @@ CREATE TABLE comment (
     rating INT NOT NULL,
     userId INT NOT NULL,
     titleId VARCHAR(256) NOT NULL,
-    ordering INT NOT NULL,
     date VARCHAR(21) NOT NULL
 );
 
 CREATE INDEX idx_userId ON comment (userId);
+
+-- hot movies
+
+CREATE TABLE hot (
+    titleId varchar(256) NOT NULL,
+    averageRating FLOAT
+);
+
+SET @lastUpdateHot = "";
+
+-- update_hot() will update hot movie relation if and only if it is not updateded today
+DELIMITER &&  
+CREATE PROCEDURE update_hot ()
+BEGIN
+    IF @lastUpdateHot < CURDATE() THEN
+    SET @lastUpdateHot = CURDATE();
+    TRUNCATE TABLE hot;
+    INSERT INTO hot(titleid, averageRating) SELECT titleid, averageRating
+    FROM titleratings
+    WHERE EXISTS (SELECT title FROM titleakas WHERE titleakas.titleId= titleratings.titleid)
+    ORDER BY titleRatings.averageRating*LOG(titleRatings.numVotes) DESC LIMIT 5;
+    END IF;
+END &&
+DELIMITER ;
+
+CALL update_hot();
